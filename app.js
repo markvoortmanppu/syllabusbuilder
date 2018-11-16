3af1925c0a48cf41be1065349fedc24116223dfc
Mark Voortman <mvoortman@pointpark.edu>
2018-11-15 23:24:22 -0500

    Increase body limit.

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.set("view engine", "hbs");
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "20mb" }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
