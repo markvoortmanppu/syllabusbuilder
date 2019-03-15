226236f03be5a1aa6a5dc7b7689174fba91d62a8
Mark Voortman <mvoortman@pointpark.edu>
2019-03-14 23:29:01 -0400

    Prepare syllabus builder for summer.

--- public/sylbuilder.js
+++ public/sylbuilder.js
@@ -51,6 +51,24 @@
     }
   }
   
+  function isMemorialDay(date) {
+    if (date.getMonth() === 4 && date.getDay() === 1 && date.getDate() >= 25 && date.getDate() <= 31) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  
+  function isFourthOfJuly(date) {
+    if (date.getMonth() === 6 && date.getDate() === 4) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  
   function newDate(str) {
     var date = new Date();
     if (str) {
@@ -67,6 +85,14 @@
       }
       if (info.CourseType === "Term II") {
         return new Date(2019, 2, 4);
+      }
+    }
+    else if (info.Semester === "Summer 2019") {
+      if (info.CourseType === "12 Week" || info.CourseType === "Term I") {
+        return new Date(2019, 4, 6);
+      }
+      if (info.CourseType === "Term II") {
+        return new Date(2019, 5, 17);
       }
     }
     return null;
@@ -105,7 +131,10 @@
     if (!alldata.syllabus.info.DoNotGenerateSchedule) {
       var date = getFirstWeek(alldata.syllabus.info);
       var duration = 15;
-      if (alldata.syllabus.info.CourseType === "Term I" || alldata.syllabus.info.CourseType === "Term II") {
+      if (alldata.syllabus.info.CourseType === "12 Week") {
+        duration = 12;
+      }
+      else if (alldata.syllabus.info.CourseType === "Term I" || alldata.syllabus.info.CourseType === "Term II") {
         duration = 8;
       }
       var alldays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
@@ -127,8 +156,27 @@
       var cnt = 0;
       var thanksgiving = false;
       var springbreak = false;
+      var minusduration = 0;
+      if (alldata.syllabus.info.Semester.startsWith("Summer") && duration === 8) {
+        if (days.indexOf("Monday") >= 0 && days.length >= 2) {
+          minusduration = 1;
+        }
+        else {
+          if (days.indexOf("Monday") >= 0 && alldata.syllabus.info.CourseType === "Term II") {
+            // only monday classes
+            date.setDate(date.getDate() + 7);
+          }
+          minusduration = 2;
+        }
+      }
+      var plusduration = 0;
+      if (alldata.syllabus.info.Semester.startsWith("Summer") && duration === 12) {
+        if (days.indexOf("Monday") >= 0 && days.length >= 2) {
+          plusduration = 1;
+        }
+      }
       if (days.length) {
-        while (cnt < duration - (duration === 8 && (thanksgiving ? 1 : 0 || springbreak ? 1 : 0))) {
+        while (cnt < duration + plusduration - (duration === 8 && (thanksgiving ? 1 : 0 || springbreak ? 1 : 0 || minusduration ? minusduration : 0))) {
           for (var i = 0; i < days.length; i++) {
             var weekpart = "";
             if (days.length > 1) {
@@ -160,17 +208,28 @@
             var tmpdate = new Date(date.getTime());
             tmpdate.setDate(tmpdate.getDate() + mapping[days[i]]);
             var datestr = (tmpdate.getMonth()+1)+"/"+tmpdate.getDate()+"/"+tmpdate.getFullYear();
-            if (isThanksgivingWeek(date)) {
+            if (alldata.syllabus.info.Semester.startsWith("Summer") && alldata.syllabus.info.CourseType === "Term II" && days.indexOf("Monday") >= 0 && days.length >= 2 && cnt === 0 && i === 0) {
+              cnt++;
+            }
+            else if (alldata.syllabus.info.Semester.startsWith("Summer") && (alldata.syllabus.info.CourseType === "Term I" || alldata.syllabus.info.CourseType === "Term II") && days.indexOf("Monday") >= 0 && days.length >= 2 && cnt === duration - minusduration && i !== 0) {
+            }
+            else if (alldata.syllabus.info.Semester.startsWith("Summer") && alldata.syllabus.info.CourseType === "12 Week" && days.indexOf("Monday") >= 0 && days.length >= 2 && cnt === duration + plusduration && i !== 0) {
+            }
+            else if (isThanksgivingWeek(date)) {
               // skip the thanksgiving week
-              template += "### Thanksgiving Break (" + datestr + ")";
-              template += "\n";
+              template += "### Thanksgiving Break (" + datestr + ")\n";
               thanksgiving = true;
             }
             else if (isSpringBreakWeek(tmpdate, getFirstWeek(alldata.syllabus.info), alldata.syllabus.info.CourseType)) {
               // skip the spring break week
-              template += "### Spring Break (" + datestr + ")";
-              template += "\n";
+              template += "### Spring Break (" + datestr + ")\n";
               springbreak = true;
+            }
+            else if (isMemorialDay(tmpdate)) {
+              template += "### Memorial Day (" + datestr + ")\n";
+              if (days.length >= 2) {
+                cnt++;
+              }
             }
             else {
               if (i === 0) {
@@ -184,6 +243,9 @@
               }
               if (isMLKDay(tmpdate)) {
                 template += "**NOTE: CLASS DOES NOT MEET DUE TO MARTIN LUTHER KING DAY**\n";
+              }
+              if (isFourthOfJuly(tmpdate)) {
+                template += "**NOTE: CLASS DOES NOT MEET DUE TO FOURTH OF JULY**\n";
               }
               template += "\{{Week" + cnt + weekpart + "Description}}\n"
               template += "\n";
